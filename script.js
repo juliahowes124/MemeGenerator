16cdb0e5acfb485520e614475ea7f3779b4f6d12
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-01-23 13:36:17 -0800

    handle invalid images

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-// https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1027&q=80
-
 let form = document.getElementById("form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  createMeme();
+  processForm();
   form.reset();
 });
 
-function createMeme() {
+function processForm() {
   let url = document.getElementById("url").value;
   let top = document.getElementById("top").value;
   let bottom = document.getElementById("bottom").value;
   let fit = document.getElementById("image-fit").value;
+  let test = new Image();
+  test.addEventListener("load", () => createMeme(url, top, bottom, fit));
+  test.addEventListener("error", imageNotFound);
+  test.src = url;
+}
 
-  if (url) {
-    let div = document.createElement("div");
-    div.className = "meme-div";
-    div.style.backgroundImage = `url(${url})`;
-    div.style.backgroundSize = fit;
-    let topText = document.createElement("p");
-    topText.innerText = top;
-    let bottomText = document.createElement("p");
-    bottomText.innerText = bottom;
-    let overlay = document.createElement("div");
-    overlay.className = "overlay";
-    let btn = document.createElement("button");
-    btn.className = "delete-btn";
-    btn.addEventListener("click", function () {
-      div.remove();
-    });
-    div.appendChild(topText);
-    div.appendChild(bottomText);
-    div.appendChild(overlay);
-    overlay.appendChild(btn);
-    btn.innerHTML = '<i class="fa fa-trash fa-3x"></i>';
+function createMeme(url, top, bottom, fit) {
+  let div = document.createElement("div");
+  div.className = "meme-div";
+  div.style.backgroundImage = `url(${url})`;
+  div.style.backgroundSize = fit;
 
-    let memes = document.getElementsByClassName("meme-section")[0];
-    memes.appendChild(div);
-  } else {
-    alert("Please enter an image url.");
-  }
+  let topText = document.createElement("p");
+  topText.innerText = top;
+  let bottomText = document.createElement("p");
+  bottomText.innerText = bottom;
+
+  let overlay = document.createElement("div");
+  overlay.className = "overlay";
+
+  let btn = document.createElement("button");
+  btn.className = "delete-btn";
+  btn.innerHTML = '<i class="fa fa-trash fa-3x"></i>';
+  btn.addEventListener("click", function () {
+    div.remove();
+  });
+
+  div.appendChild(topText);
+  div.appendChild(bottomText);
+  div.appendChild(overlay);
+  overlay.appendChild(btn);
+
+  let memes = document.getElementsByClassName("meme-section")[0];
+  memes.appendChild(div);
+}
+
+function imageNotFound() {
+  alert("Please enter a valid image url.");
 }
